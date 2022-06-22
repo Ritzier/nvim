61856db29ac75de2a2f30ba954252c0dff7e2a9f
ritzier <ritzierwheat@gmail.com>
2022-06-23 02:28:25 +0800

    Basic configuration

---
+++ after/plugin/autocmds.lua
@@ -0,0 +1,38 @@
+local api = vim.api
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank()",
+  group = yankGrp,
+})
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, { pattern = "*", command = "set cursorline", group = cursorGrp })
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd(
+  "BufReadPost",
+  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
+)
+
+-- Check if we need to reload the file when it changed
+api.nvim_create_autocmd("FocusGained", { command = [[:checktime]] })
+
+-- windows to close with "q"
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "help", "startuptime", "qf", "lspinfo" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
+)
+api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
+
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- Fix highlight issue
+api.nvim_create_autocmd("BufEnter", { command = [[syntax enable]] })
---
+++ lua/plugins.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+function M.setup()
+    -- Indicate first time installation
+    local packer_bootstrap = false
+
+    -- packer.nvim configuration
+    local conf = {
+        profile = {
+            enable = true,
+            threshold = 0 -- the amount in ms that a plugins load time must be over for it to be included in the profile
+        },
+        display = {
+            open_fn = function()
+                return require("packer.util").float {border = "rounded"}
+            end
+        }
+    }
+
+    -- Check if packer.nvim is installed
+    -- Run PackerCompile if there are changes in this file
+    local function packer_init()
+        local fn = vim.fn
+        local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+        if fn.empty(fn.glob(install_path)) > 0 then
+            packer_bootstrap =
+                fn.system {
+                "git",
+                "clone",
+                "--depth",
+                "1",
+                "https://github.com/wbthomason/packer.nvim",
+                install_path
+            }
+            vim.cmd [[packadd packer.nvim]]
+        end
+        vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+    end
+
+    -- Plugins
+    local function plugins(use)
+         -- Packer
+        use {"wbthomason/packer.nvim"}
+        
+        -- Colorscheme
+        use {
+            "EdenEast/nightfox.nvim",
+            config = function()
+                vim.cmd "colorscheme duskfox"
+            end
+        }
+
+        if packer_bootstrap then
+            print "Restart Neovim required after installation!"
+            require("packer").sync()
+        end
+    end
+    -- Init and start packer
+    packer_init()
+    local packer = require "packer"
+
+    -- Performance
+    pcall(require, "impatient")
+    -- pcall(require, "packer_compiled")
+
+    packer.init(conf)
+    packer.startup(plugins)
+end
+
+return M
