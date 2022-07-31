baa989c7347f3f08e933836aabff48af13b842ef
ritzier <ritzierwheat@gmail.com>
2022-07-31 16:16:40 +0800

    Fix endwise

--- lua/modules/autopairs.lua
+++ lua/modules/autopairs.lua
@@ -1,6 +1,4 @@
-require('nvim-autopairs').setup({
-    disable_filetype = { "TelescopePrompt" },
-    check_ts = true,
+require("nvim-autopairs").setup({
+	disable_filetype = { "TelescopePrompt" },
+	check_ts = true,
 })
-
-require('nvim-autopairs').add_rules(require "nvim-autopairs.rules.endwise-lua")
