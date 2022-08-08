af1c733f00a5c132077af47e24990c73014daa52
ritzier <ritzierwheat@gmail.com>
2022-08-08 22:30:38 +0800

    Autopairs add fast wrap

--- lua/modules/autopairs.lua
+++ lua/modules/autopairs.lua
@@ -1,4 +1,14 @@
 require("nvim-autopairs").setup({
 	disable_filetype = { "TelescopePrompt" },
 	check_ts = true,
+	fast_wrap = {
+		map = "<M-e>",
+		chars = { "{", "[", "(", '"', "'" },
+		pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+		end_key = "$",
+		keys = "qwertyuiopzxcvbnmasdfghjkl",
+		check_comma = true,
+		highlight = "Search",
+		highlight_grey = "Comment",
+	},
 })
