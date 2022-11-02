811918b7b32fd67e8229ac1d781cfb5c31fbf36f
ritzier <ritzierwheat@gmail.com>
2022-11-02 16:14:51 +0800

    Update

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -117,7 +117,27 @@ require("which-key").register({
     o = { "<cmd>LSoutlineToggle<cr>", "outline toggle" },
     d = { "<cmd>Lspsaga peek_definition<CR>", "Peek Definition" },
   },
-  t = { ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", "Neotest Run" },
+  t = {
+    name = "Test",
+    a = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach" },
+    f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Run File" },
+    F = { "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Debug File" },
+    l = { "<cmd>lua require('neotest').run.run_last()<cr>", "Run Last" },
+    L = { "<cmd>lua require('neotest').run.run_last({ strategy = 'dap' })<cr>", "Debug Last" },
+    n = { "<cmd>lua require('neotest').run.run()<cr>", "Run Nearest" },
+    N = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Nearest" },
+    o = { "<cmd>lua require('neotest').output.open({ enter = true })<cr>", "Output" },
+    S = { "<cmd>lua require('neotest').run.stop()<cr>", "Stop" },
+    s = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Summary" },
+    p = { "<Plug>PlenaryTestFile", "PlenaryTestFile" },
+    v = { "<cmd>TestVisit<cr>", "Visit" },
+    x = { "<cmd>TestSuite<cr>", "Suite" },
+    t = {
+      name = "Test",
+      l = { "<cmd>TestLast<cr>", "Run Last" },
+      n = { "<cmd>TestNearest<cr>", "Run Nearest" },
+    },
+  },
 }, { prefix = "<space>", silent = true, noremap = true })
 
 require("which-key").register({
