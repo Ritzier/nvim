b6e541513100dbd5de9681e7634f73924f66bdd5
ritzier <ritzierwheat@gmail.com>
2023-12-10 22:27:45 +0800

    Add 'C-x' as bdelete

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,58 +1,61 @@
 local wk = require("which-key")
 wk.register({
-    ["<C-h>"] = { "<C-w>h", "Focus left" },
-    ["<C-j>"] = { "<C-w>l", "Focus down" },
-    ["<C-k>"] = { "<C-w>k", "Focus up" },
-    ["<C-l>"] = { "<C-w>l", "Focus right" },
-    ["<C-n>"] = { ":NvimTreeToggle<CR>", "NvimTreeToggle" },
-    ["<A-j>"] = { ":BufferLineCycleNext<CR>", "Focus Next Buffer" },
-    ["<A-k>"] = { ":BufferLineCyclePrev<CR>", "Focus Prev Buffer" },
-    ["<A-S-j>"] = { ":BufferLineMoveNext<CR>", "Move Next Buffer" },
-    ["<A-S-k>"] = { ":BufferLineMovePrev<CR>", "Move Prev Buffer" },
+	["<C-h>"] = { "<C-w>h", "Focus left" },
+	["<C-j>"] = { "<C-w>l", "Focus down" },
+	["<C-k>"] = { "<C-w>k", "Focus up" },
+	["<C-l>"] = { "<C-w>l", "Focus right" },
+	["<C-n>"] = { ":NvimTreeToggle<CR>", "NvimTreeToggle" },
+	["<A-j>"] = { ":BufferLineCycleNext<CR>", "Focus Next Buffer" },
+	["<A-k>"] = { ":BufferLineCyclePrev<CR>", "Focus Prev Buffer" },
+	["<A-S-j>"] = { ":BufferLineMoveNext<CR>", "Move Next Buffer" },
+	["<A-S-k>"] = { ":BufferLineMovePrev<CR>", "Move Prev Buffer" },
+	["<C-x>"] = { ":bdelete<CR>", "Delete buffer" },
 }, { mode = "n", prefix = "", silent = true })
 
 wk.register({
-    ["<"] = { "<gv", "Decrease Indent" },
-    [">"] = { ">gv", "Increase Indent" },
-    ["<A-j>"] = { ":m '>+1<CR>gv=gv'", "Move line down" },
-    ["<A-k>"] = { ":m '>-2<CR>gv=gv'", "Move line up" },
-    ["ga"] = { ":'<, '>EasyAlign<CR>", "EasyAlign" },
-    ["e"] = { "<cmd>lua require('dapui').eval()<CR>", "Evaluate" },
-    ["<space>v"] = { "<cmd>GuardFmt<CR>", "Formatter" },
+	["<"] = { "<gv", "Decrease Indent" },
+	[">"] = { ">gv", "Increase Indent" },
+	["<A-j>"] = { ":m '>+1<CR>gv=gv'", "Move line down" },
+	["<A-k>"] = { ":m '>-2<CR>gv=gv'", "Move line up" },
+	["ga"] = { ":'<, '>EasyAlign<CR>", "EasyAlign" },
+	["e"] = { "<cmd>lua require('dapui').eval()<CR>", "Evaluate" },
+	["<space>v"] = { "<cmd>GuardFmt<CR>", "Formatter" },
 }, { mode = "v", prefix = "", silent = true })
 
 wk.register({
 
+	["t"] = {
+		name = "Telescope",
+		t = { "<cmd>Telescope<CR>", "Telescope" },
+		l = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
+		f = { "<cmd>Telescope find_files<CR>", "Find Files" },
+		h = { "<cmd>Telescope highlights<CR>", "Highlight" },
+	},
 
-    ["t"] = {
-        name = "Telescope",
-        t = { "<cmd>Telescope<CR>", "Telescope" },
-        l = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
-        f = { "<cmd>Telescope find_files<CR>", "Find Files" },
-        h = { "<cmd>Telescope highlights<CR>", "Highlight" }
-    },
+	["v"] = { ":lua Toggle_venn()<CR>", "Toggle Venn" },
 
-    ["v"] = { ":lua Toggle_venn()<CR>", "Toggle Venn" },
-
-    ["c"] = {
-        name = "Color",
-        c = { ":CccHighlighterToggle<CR>", "Highlighter Toggle" },
-        v = { ":CccCovert<CR>", "Convert" },
-        p = { ":CccPick<CR>", "Pick" },
-    },
-    ["f"] = { "<cmd>GuardFmt<CR>", "Formatter" },
+	["c"] = {
+		name = "Color",
+		c = { ":CccHighlighterToggle<CR>", "Highlighter Toggle" },
+		v = { ":CccCovert<CR>", "Convert" },
+		p = { ":CccPick<CR>", "Pick" },
+	},
+	["f"] = { "<cmd>GuardFmt<CR>", "Formatter" },
 }, { mode = "n", prefix = "<leader>", silent = true })
 
 wk.register({
-    ["c"] = {
-        name = "Code",
-        c = { "<cmd>CompilerOpen<CR>", "Compiler Open" },
-        r = { function()
-            vim.cmd("CompilerStop")
-            vim.cmd("CompilerRedo")
-        end, "Stop and Redo" },
-        C = { "<cmd>CompilerToggleResults<cr>", "Toggle Result" },
-    },
+	["c"] = {
+		name = "Code",
+		c = { "<cmd>CompilerOpen<CR>", "Compiler Open" },
+		r = {
+			function()
+				vim.cmd("CompilerStop")
+				vim.cmd("CompilerRedo")
+			end,
+			"Stop and Redo",
+		},
+		C = { "<cmd>CompilerToggleResults<cr>", "Toggle Result" },
+	},
 }, { mode = "n", prefix = "<space>", silent = true })
 
 -- wk.register({
