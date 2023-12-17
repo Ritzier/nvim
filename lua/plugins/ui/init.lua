f29ff9c958242908f7c92728cd3e7bb260403e80
ritzier <ritzierwheat@gmail.com>
2023-12-17 23:32:22 +0800

    Update: nvim-tree

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,35 +1,40 @@
 return {
-    {
-        "folke/edgy.nvim",
-        event = "VeryLazy",
-        opts = {
-        }
-    },
+	{
+		"folke/edgy.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
 
-    {
-        "nvim-lualine/lualine.nvim",
-        event = { "BufReadPost", "BufAdd", "BufNewFile" },
-        config = require("plugins.ui.lualine")
-    },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = { "BufReadPost", "BufAdd", "BufNewFile" },
+		config = require("plugins.ui.lualine"),
+	},
 
-    -- {
-    --     "rebelot/heirline.nvim",
-    --     event = { "BufreadPost", "BufAdd", "BufNewFile" },
-    --     config = require("plugins.ui.heirline")
-    -- },
+	-- {
+	--     "rebelot/heirline.nvim",
+	--     event = { "BufreadPost", "BufAdd", "BufNewFile" },
+	--     config = require("plugins.ui.heirline")
+	-- },
 
-    {
-        "gelguy/wilder.nvim",
-        event = "CmdlineEnter",
-        config = require("plugins.ui.wilder"),
-        dependencies = {
-            "romgrk/fzy-lua-native"
-        }
-    },
+	{
+		"gelguy/wilder.nvim",
+		event = "CmdlineEnter",
+		config = require("plugins.ui.wilder"),
+		dependencies = {
+			"romgrk/fzy-lua-native",
+		},
+	},
 
-    {
-        "lewis6991/gitsigns.nvim",
-        event = { "BufReadPost", "BufAdd", "BufNewFile" },
-        config = require("plugins.ui.gitsigns"),
-    }
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPost", "BufAdd", "BufNewFile" },
+		config = require("plugins.ui.gitsigns"),
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		event = { "BufReadPost", "BufAdd", "BufNewFile" },
+		config = require("plugins.ui.nvim_tree")
+	},
 }
