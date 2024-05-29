49bd95ab3a9a8721c7402c0eee8f807c548ef1b4
ritzier <ritzierwheat@gmail.com>
2024-05-30 02:28:31 +0800

    Del plugin: autoclose, Add plugin: autopairs

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -16,40 +16,18 @@ return {
 	},
 
 	{
-		"abecodes/tabout.nvim",
-		event = { "InsertEnter", "CmdlineEnter" },
-		-- lazy = false,
-		config = function()
-			require("tabout").setup({
-				tabkey = "", -- key to trigger tabout, set to an empty string to disable
-				backwards_tabkey = "", -- key to trigger backwards tabout, set to an empty string to disable
-
-				act_as_tab = true, -- shift content if tab out is not possible
-				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-				enable_backwards = true,
-				completion = false, -- if the tabkey is used in a completion pum
-				tabouts = {
-					{ open = "'", close = "'" },
-					{ open = '"', close = '"' },
-					{ open = "`", close = "`" },
-					{ open = "(", close = ")" },
-					{ open = "[", close = "]" },
-					{ open = "{", close = "}" },
-				},
-				ignore_beginning = true, -- if the cursor is at the beginning of a filled element it will rather tab out than shift the content
-				exclude = {}, -- tabout will ignore these filetypes
-			})
-		end,
-		requires = { "nvim-treesitter/nvim-treesitter" },
-		-- event = "InsertCharPre",
+		"windwp/nvim-autopairs",
+		event = { "Insertenter", "CmdlineEnter" },
+		config = require("plugins.editor.config.autopairs"),
 	},
 
+
 	{
-		"m4xshen/autoclose.nvim",
-		enabled = false,
-		lazy = true,
-		event = "InsertEnter",
-		config = require("plugins.editor.config.autoclose"),
+		"L3MON4D3/LuaSnip",
+		keys = function()
+			-- Disable default tab keybinding in LuaSnip
+			return {}
+		end,
 	},
 
 	{
