ab0b42827d6886b73a30039b3ebae0fc1e925c32
ritzier <ritzierwheat@gmail.com>
2024-09-01 23:38:27 +0800

    .

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -1,159 +1,161 @@
 return {
-	{
-		"olimorris/persisted.nvim",
-		lazy = true,
-		cmd = {
-			"SessionToggle",
-			"SessionStart",
-			"SessionStop",
-			"SessionSave",
-			"SessionLoad",
-			"SessionLoadLast",
-			"SessionLoadFromFile",
-			"SessionDelete",
-		},
-		config = require("plugins.editor.config.persisted"),
-	},
+    {
+        "olimorris/persisted.nvim",
+        lazy = true,
+        cmd = {
+            "SessionToggle",
+            "SessionStart",
+            "SessionStop",
+            "SessionSave",
+            "SessionLoad",
+            "SessionLoadLast",
+            "SessionLoadFromFile",
+            "SessionDelete",
+        },
+        config = require("plugins.editor.config.persisted"),
+    },
 
-	{
-		"windwp/nvim-autopairs",
-		event = { "InsertEnter", "CmdlineEnter" },
-		config = require("plugins.editor.config.autopairs"),
-		enabled = false,
-	},
+    {
+        "windwp/nvim-autopairs",
+        event = { "InsertEnter", "CmdlineEnter" },
+        config = require("plugins.editor.config.autopairs"),
+        enabled = true,
+    },
 
-	-- ERROR: Not working
-	{
-		"windwp/nvim-ts-autotag",
-		config = require("plugins.editor.config.autotag"),
-		enabled = false,
-	},
+    -- ERROR: Not working
+    {
+        "windwp/nvim-ts-autotag",
+        config = require("plugins.editor.config.autotag"),
+        enabled = false,
+    },
 
-	{
-		"m4xshen/autoclose.nvim",
-		config = require("plugins.editor.config.autoclose"),
-		event = { "InsertEnter", "CmdlineEnter" },
-	},
+    {
+        "m4xshen/autoclose.nvim",
+        config = require("plugins.editor.config.autoclose"),
+        event = { "InsertEnter", "CmdlineEnter" },
+        priority = 1000,
+        -- enabled = false
+    },
 
-	{
-		"L3MON4D3/LuaSnip",
-		keys = function()
-			-- Disable default tab keybinding in LuaSnip
-			return {}
-		end,
-	},
+    {
+        "L3MON4D3/LuaSnip",
+        keys = function()
+            -- Disable default tab keybinding in LuaSnip
+            return {}
+        end,
+    },
 
-	{
-		"LunarVim/bigfile.nvim",
-		lazy = false,
-		config = require("plugins.editor.config.bigfile"),
-	},
-	{
-		"ojroques/nvim-bufdel",
-		lazy = true,
-		cmd = { "BufDel", "BufDelAll", "BufDelOthers" },
-	},
-	{
-		"folke/flash.nvim",
-		lazy = true,
-		event = { "CursorHold", "CursorHoldI" },
-		config = require("plugins.editor.config.flash"),
-	},
+    {
+        "LunarVim/bigfile.nvim",
+        lazy = false,
+        config = require("plugins.editor.config.bigfile"),
+    },
+    {
+        "ojroques/nvim-bufdel",
+        lazy = true,
+        cmd = { "BufDel", "BufDelAll", "BufDelOthers" },
+    },
+    {
+        "folke/flash.nvim",
+        lazy = true,
+        event = { "CursorHold", "CursorHoldI" },
+        config = require("plugins.editor.config.flash"),
+    },
 
-	{
-		"numToStr/Comment.nvim",
-		enabled = false,
-		lazy = true,
-		event = { "CursorHold", "CursorHoldI" },
-		config = require("plugins.editor.config.comment"),
-	},
+    {
+        "numToStr/Comment.nvim",
+        enabled = false,
+        lazy = true,
+        event = { "CursorHold", "CursorHoldI" },
+        config = require("plugins.editor.config.comment"),
+    },
 
-	{
-		"smoka7/hop.nvim",
-		lazy = true,
-		version = "*",
-		event = { "CursorHold", "CursorHoldI" },
-		config = require("plugins.editor.config.hop"),
-		keys = {
-			{
-				"<leader>f",
-				":HopWord<CR>",
-				desc = "hop",
-			},
-		},
-	},
+    {
+        "smoka7/hop.nvim",
+        lazy = true,
+        version = "*",
+        event = { "CursorHold", "CursorHoldI" },
+        config = require("plugins.editor.config.hop"),
+        keys = {
+            {
+                "<leader>f",
+                ":HopWord<CR>",
+                desc = "hop",
+            },
+        },
+    },
 
-	{
-		"tzachar/local-highlight.nvim",
-		enabled = false,
-		lazy = true,
-		event = { "CursorHold", "CursorHoldI" },
+    {
+        "tzachar/local-highlight.nvim",
+        enabled = false,
+        lazy = true,
+        event = { "CursorHold", "CursorHoldI" },
 
-		config = require("plugins.editor.config.local-highlight"),
-	},
+        config = require("plugins.editor.config.local-highlight"),
+    },
 
-	{
-		"romainl/vim-cool",
-		lazy = true,
-		event = { "CursorMoved", "InsertEnter" },
-	},
+    {
+        "romainl/vim-cool",
+        lazy = true,
+        event = { "CursorMoved", "InsertEnter" },
+    },
 
-	{
-		"lambdalisue/suda.vim",
-		lazy = true,
-		cmd = { "SudaRead", "SudaWrite" },
-		config = require("plugins.editor.config.suda"),
-	},
+    {
+        "lambdalisue/suda.vim",
+        lazy = true,
+        cmd = { "SudaRead", "SudaWrite" },
+        config = require("plugins.editor.config.suda"),
+    },
 
-	{
-		"tpope/vim-sleuth",
-		lazy = true,
-		event = { "BufNewFile", "BufReadPost", "BufFilePost" },
-	},
+    {
+        "tpope/vim-sleuth",
+        lazy = true,
+        event = { "BufNewFile", "BufReadPost", "BufFilePost" },
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = true,
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		build = ":TSUpdate",
-		-- config = function()
-		-- 	require("nvim-treesitter.configs").setup({
-		-- 		endwise = { enable = true }
-		-- 	})
-		-- end,
-		config = require("plugins.editor.config.treesitter"),
-		dependencies = {
-			{ "RRethy/nvim-treesitter-endwise" },
-			{ "mfussenegger/nvim-treehopper" },
-			{ "nvim-treesitter/nvim-treesitter-textobjects" },
-			{
-				"NvChad/nvim-colorizer.lua",
-				config = require("plugins.editor.config.colorizer"),
-			},
-			{
-				"hiphish/rainbow-delimiters.nvim",
-				config = require("plugins.editor.config.rainbow_delims"),
-			},
-			{
-				"nvim-treesitter/nvim-treesitter-context",
-				enabled = false,
-				config = require("plugins.editor.config.ts-context"),
-			},
-			{
-				"JoosepAlviste/nvim-ts-context-commentstring",
-				enabled = false,
-				config = require("plugins.editor.config.ts-context-commentstring"),
-			},
-		},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = true,
+        event = { "BufReadPost", "BufNewFile" },
+        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+        build = ":TSUpdate",
+        -- config = function()
+        -- 	require("nvim-treesitter.configs").setup({
+        -- 		endwise = { enable = true }
+        -- 	})
+        -- end,
+        config = require("plugins.editor.config.treesitter"),
+        dependencies = {
+            { "RRethy/nvim-treesitter-endwise" },
+            { "mfussenegger/nvim-treehopper" },
+            { "nvim-treesitter/nvim-treesitter-textobjects" },
+            {
+                "NvChad/nvim-colorizer.lua",
+                config = require("plugins.editor.config.colorizer"),
+            },
+            {
+                "hiphish/rainbow-delimiters.nvim",
+                config = require("plugins.editor.config.rainbow_delims"),
+            },
+            {
+                "nvim-treesitter/nvim-treesitter-context",
+                enabled = false,
+                config = require("plugins.editor.config.ts-context"),
+            },
+            {
+                "JoosepAlviste/nvim-ts-context-commentstring",
+                enabled = false,
+                config = require("plugins.editor.config.ts-context-commentstring"),
+            },
+        },
+    },
 
-	{
-		"max397574/better-escape.nvim",
-		lazy = true,
-		event = { "InsertEnter" },
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
+    {
+        "max397574/better-escape.nvim",
+        lazy = true,
+        event = { "InsertEnter" },
+        config = function()
+            require("better_escape").setup()
+        end,
+    },
 }
