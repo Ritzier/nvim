a6bb9bd15bafcf12a2aa10135c4df9c7d5d86172
ritzier <ritzierwheat@gmail.com>
2024-10-23 16:58:33 +0800

    modify

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 require("config.options")
 require("config.lazy")
 require("config.autocmds")
---require("utils.contextmenu")
+require("keymaps")
 
-require('keymaps')
--- vim.cmd([[colorscheme tokyonight]])
 vim.cmd([[colorscheme catppuccin]])
-
 
 vim.o.autoread = true
