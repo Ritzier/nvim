981cfc6744a3489d6e5cbeec9893b6d56cdc2b1a
ritzier <ritzierwheat@gmail.com>
2024-10-23 16:55:17 +0800

    feat(keymap): simplify code

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,26 +1,6 @@
-local wk = require("which-key")
-
-wk.add({
-	{
-		mode = { "n" },
-		{ "<C-h>", "<C-w>h", desc = "Focus left" },
-		{ "<C-j>", "<C-w>j", desc = "Focus down" },
-		{ "<C-k>", "<C-w>k", desc = "Focus up" },
-		{ "<C-l>", "<C-w>l", desc = "Focus right" },
-		{ "<C-x>", ":bdelete<CR>", desc = "Delete buffer" },
-		{ "<leader>d", group = "DiffView" },
-		{ "<leader>t", group = "Telescope" },
-		{ "<space>d", group = "Doc View" },
-		{ "<space>l", group = "Lsp" },
-	},
-
-	{
-		mode = { "v" },
-		{ "<", "<gv", desc = "Decrease Indent" },
-		{ ">", ">gv", desc = "Increase Indent" },
-		{ "<A-j>", ":m '>+1<CR>gv=gv'", desc = "Move line down" },
-		{ "<A-k>", ":m '>-2<CR>gv=gv'", desc = "Move line up" },
-		-- { "ga", ":'<,'>EasyAlign<CR>", desc = "EasyAlign" },
-		{ "s", ":'<,'>Sort u<CR>", desc = "Sort" },
-	},
-})
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus up" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right" })
+vim.keymap.set("v", "<", "<gv", { desc = "Decrease indent" })
+vim.keymap.set("v", ">", ">gv", { desc = "Increase indent" })
