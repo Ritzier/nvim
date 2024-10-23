f9876c74746b08e08fe8214120d8ff9a7cfd7130
ritzier <ritzierwheat@gmail.com>
2024-10-23 16:13:44 +0800

    feat(tool): modify `folke/which-key.nvim`

--- lua/plugins/tool/config/which-key.lua
+++ lua/plugins/tool/config/which-key.lua
@@ -1,5 +1,26 @@
 return function()
 	require("which-key").setup({
 		preset = "classic",
+		triggers = {
+			{ "<auto>", mode = "nixso" },
+		},
+		plugins = {
+			marks = true,
+			registers = true,
+			spelling = {
+				enabled = true,
+				suggestions = 20,
+			},
+			presets = {
+				motions = false,
+				operators = false,
+				text_objects = true,
+				windows = true,
+				nav = true,
+				z = true,
+				g = true,
+			},
+		},
+		win = { border = "none", padding = { 1, 2 }, wo = { winblend = 0 } },
 	})
 end
