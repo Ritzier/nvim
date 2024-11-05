862f94c67130278b077f8c59e104e451b5fd69d0
ritzier <ritzierwheat@gmail.com>
2024-11-05 09:25:02 +0800

    feat(config): remove unused configuration * remove `python_host_prog` use system defualt

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -104,33 +104,6 @@ local global_local = {
 	writebackup = false,
 }
 
-local function isempty(s)
-	return s == nil or s == ""
-end
-local function use_if_defined(val, fallback)
-	return val ~= nil and val or fallback
-end
-
--- -- custom python provider
--- local conda_prefix = os.getenv("CONDA_PREFIX")
--- if not isempty(conda_prefix) then
--- 	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
--- 	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
--- else
--- 	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
--- 	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
--- end
-
-vim.g.python_host_prog = "/home/ritzier/python312/bin/python"
-vim.g.python3_host_prog = "/home/ritzier/python312/bin/python"
-
--- custom sqlite3 provider
-local sqlite_clib_path = os.getenv("SQLITE_CLIB_PATH")
-if not isempty(sqlite_clib_path) then
-	-- Try environment variable first
-	vim.g.sqlite_clib_path = sqlite_clib_path
-end
-
 for name, value in pairs(global_local) do
 	vim.o[name] = value
 end
