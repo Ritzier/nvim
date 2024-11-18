6026945d63d5d2cb7b763ea80d16667f73ac7925
ritzier <ritzierwheat@gmail.com>
2024-11-18 13:30:18 +0800

    feat: modify plugins

--- lua/plugins/editor/config/autopairs.lua
+++ lua/plugins/editor/config/autopairs.lua
@@ -5,25 +5,23 @@ return function()
 
 	npairs.setup({
 		check_ts = true,
+		map_cr = true,
 	})
 
-	npairs.add_rule(Rule("<", ">"):with_pair(function(opts)
+	-- Existing Rust-specific rule
+	npairs.add_rule(Rule("<", ">", "rust"):with_pair(function(opts)
 		local bufnr = opts.bufnr
 
-		-- Function to check if is inside a view! macro
 		local function is_in_view_macro()
 			local current_linenr = vim.api.nvim_win_get_cursor(0)[1]
 
-			-- Search backwards for view! macro start
 			for i = current_linenr, 1, -1 do
 				local line_content = vim.api.nvim_buf_get_lines(bufnr, i - 1, i, false)[1]
 
-				-- Check if line contains view! macro start
 				if line_content:match("view!%s*{") then
 					return true
 				end
 
-				-- Stop searching if we find a closing brace that's not part of the macro
 				if line_content:match("%s*}") then
 					break
 				end
@@ -32,12 +30,10 @@ return function()
 			return false
 		end
 
-		-- Start checking view! macro
 		if is_in_view_macro() then
 			return false
 		end
 
-		-- Check is it start with if and match
 		if opts.line:match("^%s*if%s+") or opts.line:match("^%s*match%s+") then
 			return false
 		end
--- lua/plugins/editor/config/autotag.lua
+++ lua/plugins/editor/config/autotag.lua
@@ -2,17 +2,14 @@ return function()
 	require("nvim-ts-autotag").setup({
 		opts = {
 			-- Defaults
-			enable_close = false, -- Auto close tags
+			enable_close = true, -- when true, it will overwrite the autopair <> configuration
 			enable_rename = true, -- Auto rename pairs of tags
 			enable_close_on_slash = false, -- Auto close on trailing </
 		},
-		-- Also override individual filetype configs, these take priority.
-		-- Empty by default, useful if one of the "opts" global settings
-		-- doesn't work well in a specific filetype
-		-- per_filetype = {
-		-- 	["html"] = {
-		-- 		enable_close = false,
-		-- 	},
-		-- },
+		per_filetype = {
+			["rust"] = {
+				enable_close = false,
+			},
+		},
 	})
 end
