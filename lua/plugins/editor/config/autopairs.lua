92bab0cd93fc91b18911e88a3762166129493e1a
ritzier <ritzierwheat@gmail.com>
2024-11-18 14:43:50 +0800

    feat(editor): `autopairs` add fast_wrap

--- lua/plugins/editor/config/autopairs.lua
+++ lua/plugins/editor/config/autopairs.lua
@@ -6,6 +6,19 @@ return function()
 	npairs.setup({
 		check_ts = true,
 		map_cr = true,
+		fast_wrap = {
+			map = "<M-e>",
+			chars = { "{", "[", "(", '"', "'" },
+			pattern = [=[[%'%"%>%]%)%}%,]]=],
+			end_key = "$",
+			before_key = "h",
+			after_key = "l",
+			cursor_pos_before = true,
+			keys = "qwertyuiopzxcvbnmasdfghjkl",
+			manual_position = true,
+			highlight = "Search",
+			highlight_grey = "Comment",
+		},
 	})
 
 	-- Existing Rust-specific rule
