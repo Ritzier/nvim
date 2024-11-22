4c0af2444492a70467b8fbb9642a907519589103
ritzier <ritzierwheat@gmail.com>
2024-11-23 04:20:51 +0800

    feat: modify `autotag`

--- lua/plugins/editor/config/autotag.lua
+++ lua/plugins/editor/config/autotag.lua
@@ -2,17 +2,17 @@ return function()
 	require("nvim-ts-autotag").setup({
 		opts = {
 			-- Defaults
-			enable_close = true, -- when true, it will overwrite the autopair <> configuration
+			enable_close = false, -- when true, it will overwrite the autopair <> configuration
 			enable_rename = true, -- Auto rename pairs of tags
 			enable_close_on_slash = true, -- Auto close on trailing </
 		},
-		-- per_filetype = {
-		-- 	["html"] = {
-		-- 		enable_close = true,
-		-- 	},
-		-- 	["rust"] = {
-		-- 		enable_close = true,
-		-- 	},
-		-- },
+		per_filetype = {
+			["html"] = {
+				enable_close = true,
+			},
+			["rust"] = {
+				enable_close = false,
+			},
+		},
 	})
 end
