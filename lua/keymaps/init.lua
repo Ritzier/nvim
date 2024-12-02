6a85003e8f3906ac8a7397279f9894b429b2be7e
ritzier <ritzierwheat@gmail.com>
2024-12-03 06:55:45 +0800

    feat: rmove unsued keymap and function, add keymap for <Shift+Enter>

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -8,22 +8,4 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 vim.keymap.set("n", "<Esc>", "<Esc>:noh<CR>", { silent = true })
 vim.keymap.set("i", "<Esc>", "<Esc>", { silent = true })
-
-local ts_utils = require("nvim-treesitter.ts_utils")
-
-local function print_node_type_at_cursor()
-	local node = ts_utils.get_node_at_cursor()
-	if not node then
-		print("No Tree-sitter node found at cursor.")
-		return
-	end
-
-	local node_type = node:type()
-	print("Node type under cursor:", node_type)
-end
-
--- Map a command to call this function
-vim.api.nvim_create_user_command("PrintNodeType", print_node_type_at_cursor, {})
-
-vim.keymap.set("i", "<F1>", "<cmd>PrintNodeType<CR>", { silent = true })
-vim.keymap.set("n", "<F1>", "<cmd>PrintNodeType<CR>", { silent = true })
+vim.keymap.set("i", "<S-CR>", "<C-o>o<Esc>O", { noremap = true, silent = true })
