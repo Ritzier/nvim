797e74dd50bae7f68dd00672537171e1dadb2cfb
ritzier <ritzierwheat@gmail.com>
2024-12-14 19:12:55 +0800

    feat(completion): enabled `handlers` for auto configuration

--- lua/plugins/completion/config/mason-null-ls.lua
+++ lua/plugins/completion/config/mason-null-ls.lua
@@ -5,5 +5,6 @@ return function()
 	require("mason-null-ls").setup({
 		ensure_installed = require("plugins.completion.server_list")["null_ls"],
 		automatic_installation = true,
+		handlers = {},
 	})
 end
