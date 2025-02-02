ca5af9050a0f82d89eb542223f026265f2591c0d
ritzier <ritzierwheat@gmail.com>
2025-02-03 07:57:07 +0800

    feat(keymap): map `U` as `redo`

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -11,3 +11,5 @@ vim.keymap.set("i", "<Esc>", "<Esc>", { silent = true })
 vim.keymap.set("i", "<S-CR>", "<CR><Esc>O", { noremap = true, silent = true })
 vim.keymap.set("i", "<A-CR>", "<Esc>O", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-CR>", "<Esc>o", { noremap = true, silent = true })
+
+vim.keymap.set("n", "U", ":redo<CR>", { noremap = true, silent = true })
