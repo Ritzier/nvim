6c827c2adf0d58d9dbb97f8b26598eea2ed7836e
ritzier <ritzierwheat@gmail.com>
2025-02-08 00:57:48 +0800

    feat(keymap): add `<Shift-h>` mapping

--- lua/keymaps/mtgap.lua
+++ lua/keymaps/mtgap.lua
@@ -8,5 +8,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><Down>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-t>", "<C-w><Up>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-s>", "<C-w><Right>", { noremap = true, silent = true })
 
+vim.keymap.set("n", "<S-h>", "<S-j>")
+
 vim.keymap.set("v", "H", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "T", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
