4df71d6958cb0856cd8061760ad5a63f279c6305
ritzier <ritzierwheat@gmail.com>
2025-02-07 08:27:00 +0800

    feat(keymaps)

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,11 +1,5 @@
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus down" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus up" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right" })
 vim.keymap.set("v", "<", "<gv", { desc = "Decrease indent" })
 vim.keymap.set("v", ">", ">gv", { desc = "Increase indent" })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 vim.keymap.set("n", "<Esc>", "<Esc>:noh<CR>", { silent = true })
 vim.keymap.set("i", "<Esc>", "<Esc>", { silent = true })
 vim.keymap.set("i", "<S-CR>", "<CR><Esc>O", { noremap = true, silent = true })
@@ -13,3 +7,5 @@ vim.keymap.set("i", "<A-CR>", "<Esc>O", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-CR>", "<Esc>o", { noremap = true, silent = true })
 
 vim.keymap.set("n", "U", ":redo<CR>", { noremap = true, silent = true })
+
+require("keymaps.mtgap")
--- lua/keymaps/mtgap.lua
+++ lua/keymaps/mtgap.lua
@@ -7,3 +7,6 @@ vim.keymap.set("n", "<C-m>", "<C-w><Left>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-h>", "<C-w><Down>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-t>", "<C-w><Up>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-s>", "<C-w><Right>", { noremap = true, silent = true })
+
+vim.keymap.set("v", "H", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "T", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
