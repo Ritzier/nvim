c6797e72eb01f759472663ad7b253e2049a2461b
ritzier <ritzierwheat@gmail.com>
2025-07-29 03:56:45 +0800

    feat(completion): remove `trouble`

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -131,51 +131,6 @@ return {
 	},
 
 	{
-		"folke/trouble.nvim",
-		lazy = true,
-		cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
-		event = "CmdlineEnter",
-		config = require("plugins.completion.config.trouble"),
-		keys = {
-			{
-				"<leader>xx",
-				"<cmd>Trouble diagnostics toggle<cr>",
-				desc = "Diagnostics (Trouble)",
-			},
-			{
-				"<leader>xX",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-				desc = "Buffer Diagnostics (Trouble)",
-			},
-			{
-				"<leader>cs",
-				"<cmd>Trouble symbols toggle focus=false<cr>",
-				desc = "Symbols (Trouble)",
-			},
-			{
-				"<leader>cl",
-				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-				desc = "LSP Definitions / references / ... (Trouble)",
-			},
-			{
-				"<leader>xL",
-				"<cmd>Trouble loclist toggle<cr>",
-				desc = "Location List (Trouble)",
-			},
-			{
-				"<leader>xQ",
-				"<cmd>Trouble qflist toggle<cr>",
-				desc = "Quickfix List (Trouble)",
-			},
-			{
-				"<leader>D",
-				"<cmd>Trouble diagnostics win = { type = split, position=right}",
-				desc = "Show workspace diagnostic",
-			},
-		},
-	},
-
-	{
 		"saghen/blink.cmp",
 		lazy = true,
 		event = "InsertEnter",
