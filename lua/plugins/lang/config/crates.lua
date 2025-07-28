b990197eedb0c80421f01fed6e32d9686f66813c
ritzier <ritzierwheat@gmail.com>
2025-07-28 20:48:52 +0800

    feat(lang): `crates` removed `cmp` requirement

--- lua/plugins/lang/config/crates.lua
+++ lua/plugins/lang/config/crates.lua
@@ -1,13 +1,4 @@
 return function()
-	-- TODO: `cmp` is replaced by `blink`
-	-- vim.api.nvim_create_autocmd("BufRead", {
-	-- 	group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-	-- 	pattern = "Cargo.toml",
-	-- 	callback = function()
-	-- 		require("cmp").setup.buffer({ sources = { { name = "crates" } } })
-	-- 	end,
-	-- })
-
 	require("crates").setup({
 		smart_insert = true,
 		insert_closing_quote = true,
@@ -75,9 +66,7 @@ return function()
 				loading = " " .. "",
 			},
 		},
-		completion = {
-			cmp = { enabled = true },
-		},
+
 		lsp = {
 			enabled = true,
 			actions = true,
