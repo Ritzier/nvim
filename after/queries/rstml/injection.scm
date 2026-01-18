8c6ce472e8dd1794359614fdd8bb6c90f15ffeb3
ritzier <ritzierwheat@gmail.com>
2026-01-19 00:09:53 +0800

    feat: add `rainbow-delimiters` support for rstml with HTML tag highlighting
    - Add  with Rust delimiter patterns
    - Include HTML `element_node` and `self_closing_element_node` queries
    - Highlight `opening/closing` tags with `node_identifier` captures

---
+++ after/queries/rstml/injection.scm
@@ -0,0 +1,155 @@
+(parenthesized_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(declaration_list
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(field_declaration_list
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(ordered_field_declaration_list
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(enum_variant_list
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(use_list
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(field_initializer_list
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(parameters
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(arguments
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(block
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(match_block
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(tuple_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(tuple_type
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(token_tree
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(token_tree
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(token_tree
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(token_tree_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(token_repetition_pattern
+  "$" @delimiter
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(token_repetition
+  "$" @delimiter
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(attribute_item
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(inner_attribute_item
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(type_arguments
+  "<" @delimiter
+  ">" @delimiter @sentinel) @container
+
+(type_parameters
+  "<" @delimiter
+  ">" @delimiter @sentinel) @container
+
+(closure_parameters
+  "|" @delimiter
+  (_)?
+  "|" @delimiter @sentinel) @container
+
+(array_expression
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(array_type
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(index_expression
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(tuple_struct_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(tuple_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(struct_pattern
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(slice_pattern
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(macro_definition
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(visibility_modifier
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(bracketed_type
+  "<" @delimiter
+  ">" @delimiter @sentinel) @container
+
+; HTML element nodes - matching open and close tags with tag names
+(element_node
+  open_tag: (open_tag
+    "<" @delimiter
+    name: (node_identifier) @delimiter
+    ">" @delimiter @sentinel)
+  close_tag: (close_tag
+    "</" @delimiter
+    name: (node_identifier) @delimiter
+    ">" @delimiter @sentinel)) @container
+
+; Self-closing HTML tags with tag names
+(self_closing_element_node
+  "<" @delimiter
+  name: (node_identifier) @delimiter
+  "/>" @delimiter @sentinel) @container
