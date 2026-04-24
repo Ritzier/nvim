1ef0a6ac3e4c2d81ccf52e82a04ef4808b4ef76c
ritzier <ritzier@gmail.com>
2026-04-25 07:48:14 +0800

    fix(ui): `lualine` remove overseer component

--- lua/plugins/ui/config/lualine.lua
+++ lua/plugins/ui/config/lualine.lua
@@ -41,23 +41,6 @@ return function()
 			},
 			lualine_c = {
 				{
-					"overseer",
-					label = "", -- Prefix for task counts
-					colored = true, -- Color the task icons and counts
-					symbols = {
-						[overseer.STATUS.FAILURE] = "F:",
-						[overseer.STATUS.CANCELED] = "C:",
-						[overseer.STATUS.SUCCESS] = "S:",
-						[overseer.STATUS.RUNNING] = "R:",
-					},
-					unique = false, -- Unique-ify non-running task count by name
-					name = nil, -- List of task names to search for
-					name_not = false, -- When true, invert the name search
-					status = nil, -- List of task statuses to display
-					status_not = false, -- When true, invert the status search
-				},
-
-				{
 					function()
 						local ok, bar = pcall(require("lspsaga.symbol.winbar").get_bar)
 						if not ok or bar == nil then
